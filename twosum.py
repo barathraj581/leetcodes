78fbd952713feca7714e185c90c9fdcc29a65480
Barathraj M <barath86296@gmail.com>
2026-06-26 13:28:38 +0530

    twosum prog

--- twosum.py
+++ twosum.py
@@ -1,1 +1,9 @@
-hrthrhf
+x=[2,7,11,15]
+target=9
+def two_sum(x,target):
+    y=len(x)
+    for i in range(y):
+        for j in range(i+1,y):
+            if x[i]+x[j] == target:
+                return [i,j]
+print(two_sum(x,target))
